e1232ab0a5a3c3ced1143fa82c97fc366bf8c331
Morgiver <morgan@morgiver.net>
2020-09-12 08:49:01 +0200

    Move RootApplication class into Core Package

--- Core/__init__.py
+++ Core/__init__.py
@@ -1,3 +1,5 @@
+import sys
+
 
 class AbstractPackage:
     def __init__(self, root_app, name):
@@ -10,4 +12,46 @@ class AbstractPackage:
 
     def require(self, package_name):
         return self.root_app.require(package_name)
+
+
+class RootApplication:
+    def __init__(self):
+        self.packages = {}
+
+        self.register_package("State")
+        self.register_package("Exchanges")
+        self.register_package("Database")
+        self.register_package("HistoricData")
+        self.register_package("AI")
+        self.register_package("UI")
+
+        self.instantiate_package()
+
+    @staticmethod
+    def __import__(package_name):
+        try:
+            package = __import__(package_name)
+            main_class = getattr(package, f'{package_name}Package')
+            return main_class
+        except ModuleNotFoundError:
+            print(f"{package_name} does not exist")
+        except:
+            print("Unexpected error :", sys.exc_info()[0])
+
+    def register_package(self, package_name):
+        package_class = self.__import__(package_name)
+        package = package_class(self)
+        self.packages[package.name] = package
+
+    def instantiate_package(self):
+        for package in self.packages:
+            self.packages[package].instantiate()
+
+    def require(self, package_name):
+        try:
+            return self.packages[package_name]
+        except KeyError:
+            print(f"Package Error : [{package_name}] is not Registred")
+        except:
+            print("Unexpected error :", sys.exc_info()[0])
 
